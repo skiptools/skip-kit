05a359b169b1eb6f99b32623cd5afa3b36a5ccb2
Marc Prud'hommeaux <marc@skip.tools>
2025-02-20 12:06:42 -0500

    Disable bridging for now

--- Sources/SkipKit/SkipKit.swift
+++ Sources/SkipKit/SkipKit.swift
@@ -2,7 +2,7 @@
 // under the terms of the GNU Lesser General Public License 3.0
 // as published by the Free Software Foundation https://fsf.org
 #if !SKIP_BRIDGE
-#if SKIP || canImport(OSLog)
+#if canImport(OSLog)
 import OSLog
 #endif
 
