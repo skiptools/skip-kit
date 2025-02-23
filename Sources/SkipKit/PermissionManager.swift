67fbab610e63aa315abd5fcb8ee42b8aacf8ed6f
Marc Prud'hommeaux <marc@skip.tools>
2025-02-23 07:42:17 -0500

    Add individual permission request functions

--- Sources/SkipKit/PermissionManager.swift
+++ Sources/SkipKit/PermissionManager.swift
@@ -5,6 +5,10 @@
 import Foundation
 import SwiftUI
 #if !SKIP
+import Photos
+import Contacts
+import EventKit
+import AVFoundation
 import SystemConfiguration
 #else
 import android.Manifest
@@ -26,30 +30,177 @@ public class PermissionManager {
     private init() {
     }
 
-    /// Requests the given permission type
-    public static func requestPermission(_ permissionName: String) async -> Bool {
+    /// Requests the given permission.
+    /// - Parameters:
+    ///   - permission: the permission, such as `PermissionType.CAMERA`
+    ///   - showRationale: an optional async callback to invoke when the system determies that a rationale should be displayed for the permission check
+    /// - Returns: true if the permission was granted, false if denied or there was an error making the request
+    public static func requestPermission(_ permission: PermissionType, showRationale: (() async -> Bool)? = nil) async -> Bool {
         #if !SKIP
-        return await withCheckedContinuation { continuation in
-            // TODO: iOS side
-//            self.callback = { result in
-//                switch result {
-//                case .success(let location):
-//                    continuation.resume(returning: location)
-//                    self.callback = nil
-//                case .failure(let error):
-//                    continuation.resume(throwing: error)
-//                    self.callback = nil
-//                }
-//            }
-//            requestLocationOrAuthorization()
-
-            continuation.resume(returning: true)
+        switch permission {
+        case .POST_NOTIFICATIONS: return await (try? requestPostNotificationPermission()) == true
+        case .READ_CONTACTS: return await (try? requestContactsPermission(readWrite: false)) == true
+        case .WRITE_CONTACTS: return await (try? requestContactsPermission(readWrite: true)) == true
+        case .READ_CALENDAR: return await (try? requestCalendarPermission(readWrite: false)) == true
+        case .WRITE_CALENDAR: return await (try? requestCalendarPermission(readWrite: true)) == true
+        case .READ_EXTERNAL_STORAGE: return await requestPhotoLibraryPermission(readWrite: false) == true
+        case .WRITE_EXTERNAL_STORAGE: return await requestPhotoLibraryPermission(readWrite: true) == true
+        case .RECORD_AUDIO: return await requestRecordAudioPermission() == true
+        case .CAMERA: return await requestCameraPermission() == true
+        default: return true
         }
         #else
         // e.g.: android.permission.ACCESS_FINE_LOCATION
-        try await UIApplication.shared.requestPermission("android.permission." + permissionName)
+        try await UIApplication.shared.requestPermission(permission.androidPermissionName, showRationale: showRationale)
         #endif
     }
+
+    /// Requests permission to send push notifications
+    public static func requestPostNotificationPermission(alert: Bool = true, sound: Bool = true, badge: Bool = true) async throws -> Bool {
+        #if SKIP
+        return await requestPermission(.POST_NOTIFICATIONS)
+        #else
+        var opts = UNAuthorizationOptions()
+        if alert { opts.insert(.alert) }
+        if sound { opts.insert(.sound) }
+        if badge { opts.insert(.badge) }
+        return try await UNUserNotificationCenter.current().requestAuthorization(options: opts)
+        #endif
+    }
+
+    /// Request permission to use the device camera
+    public static func requestCameraPermission() async -> Bool {
+        #if SKIP
+        return await requestPermission(.CAMERA)
+        #else
+        return await requestAVPermission(for: .video)
+        #endif
+    }
+
+    /// Requests microphone access
+    public static func requestRecordAudioPermission() async -> Bool {
+        #if SKIP
+        return await requestPermission(.RECORD_AUDIO)
+        #else
+        return await requestAVPermission(for: .audio)
+        #endif
+    }
+
+    #if !SKIP
+    private static func requestAVPermission(for mediaType: AVMediaType) async -> Bool {
+        let status = AVCaptureDevice.authorizationStatus(for: mediaType)
+
+        switch status {
+        case .authorized:
+            return true
+        case .notDetermined:
+            return await AVCaptureDevice.requestAccess(for: mediaType)
+        case .denied, .restricted:
+            return false
+        @unknown default:
+            return false
+        }
+    }
+    #endif
+
+    public static func requestContactsPermission(readWrite: Bool = false) async throws -> Bool {
+        #if SKIP
+        return await requestPermission(readWrite ? .WRITE_CONTACTS : .READ_CONTACTS)
+        #else
+        let contactStore = CNContactStore()
+        let contactAuthorizationStatus = CNContactStore.authorizationStatus(for: .contacts)
+
+        if contactAuthorizationStatus == .notDetermined {
+            return try await contactStore.requestAccess(for: .contacts)
+        } else if contactAuthorizationStatus == .denied {
+            return false
+        }
+        return true
+        #endif
+    }
+
+    public static func requestCalendarPermission(readWrite: Bool = false) async throws -> Bool {
+        #if SKIP
+        return await requestPermission(readWrite ? .WRITE_CALENDAR : .READ_CALENDAR)
+        #else
+        return try await requestEventKitPermission(for: .event)
+        #endif
+    }
+
+    public static func requestReminderPermission(readWrite: Bool = false) async throws -> Bool {
+        #if SKIP
+        return await requestPermission(readWrite ? .WRITE_CALENDAR : .READ_CALENDAR)
+        #else
+        return try await requestEventKitPermission(for: .reminder)
+        #endif
+    }
+
+    #if !SKIP
+    private static func requestEventKitPermission(for eventType: EKEntityType) async throws -> Bool {
+        let status = EKEventStore.authorizationStatus(for: eventType)
+        switch status {
+        case .notDetermined:
+            let eventStore = EKEventStore()
+            return try await eventStore.requestAccess(to: eventType)
+        case .authorized:
+            return true
+        case .denied:
+            return false
+        default:
+            return true // e.g. restricted
+        }
+    }
+    #endif
+
+    /// Requests the media library permission
+    public static func requestPhotoLibraryPermission(readWrite: Bool = true) async -> Bool {
+        #if SKIP
+        return await requestPermission(readWrite ? .WRITE_EXTERNAL_STORAGE : .READ_EXTERNAL_STORAGE)
+        #else
+        switch PHPhotoLibrary.authorizationStatus(for: readWrite ? .readWrite : .addOnly) {
+        case .authorized, .limited:
+            return true
+        case .notDetermined:
+            let status = await PHPhotoLibrary.requestAuthorization(for: readWrite ? .readWrite : .addOnly)
+            return status == .authorized || status == .limited
+        default:
+            return false
+        }
+        #endif
+    }
+}
+
+/// The encapsulation of a permission name
+public struct PermissionType : Equatable {
+    public let androidPermissionName: String
+
+    public init(androidPermissionName: String) {
+        self.androidPermissionName = androidPermissionName
+    }
+
+    public static func == (lhs: PermissionType, rhs: PermissionType) -> Bool {
+        lhs.androidPermissionName == rhs.androidPermissionName
+    }
+}
+
+/// https://developer.android.com/reference/android/Manifest.permission
+public extension PermissionType {
+    static let CAMERA = PermissionType(androidPermissionName: "android.permission.CAMERA")
+    static let RECORD_AUDIO = PermissionType(androidPermissionName: "android.permission.RECORD_AUDIO")
+
+    static let READ_CONTACTS = PermissionType(androidPermissionName: "android.permission.READ_CONTACTS")
+    static let WRITE_CONTACTS = PermissionType(androidPermissionName: "android.permission.WRITE_CONTACTS")
+
+    static let READ_CALENDAR = PermissionType(androidPermissionName: "android.permission.READ_CALENDAR")
+    static let WRITE_CALENDAR = PermissionType(androidPermissionName: "android.permission.WRITE_CALENDAR")
+
+    // API 11+ breaks this up into: READ_MEDIA_IMAGES, READ_MEDIA_VIDEO
+    static let READ_EXTERNAL_STORAGE = PermissionType(androidPermissionName: "android.permission.READ_EXTERNAL_STORAGE")
+    static let WRITE_EXTERNAL_STORAGE = PermissionType(androidPermissionName: "android.permission.WRITE_EXTERNAL_STORAGE")
+
+    static let POST_NOTIFICATIONS = PermissionType(androidPermissionName: "android.permission.POST_NOTIFICATIONS")
+    static let ACCESS_FINE_LOCATION = PermissionType(androidPermissionName: "android.permission.ACCESS_FINE_LOCATION")
+    static let ACCESS_COARSE_LOCATION = PermissionType(androidPermissionName: "android.permission.ACCESS_COARSE_LOCATION")
 }
 
 #endif
